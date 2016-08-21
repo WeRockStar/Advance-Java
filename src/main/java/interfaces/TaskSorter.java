ad46504ff3e94c9e4d982ad88b6bfc7260876000
WeRockStar <vr_freestyle@outlook.com>
2016-08-21 22:27:28 +0700

    Added Task class

---
+++ src/main/java/interfaces/TaskSorter.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskSorter {
+    public static void main(String []args) {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1, "Run away killer rabbit", 4));
+        tasks.add(new Task(2, "Keep pokemon at Siam paragon", 1));
+        tasks.add(new Task(3, "Evolve pokemon", 3));
+    }
+}

b12988fd8a40011845249aadbcaeafb138c49147
WeRockStar <vr_freestyle@outlook.com>
2016-08-21 21:17:53 +0700

    Added Hourly class

--- src/main/java/oop/UseHR.java
+++ src/main/java/oop/UseHR.java
@@ -1,7 +1,10 @@
 package oop;
 
 public class UseHR {
-    public static void main() {
+    public static void main(String[] args) {
         HR hr = new HR();
+        hr.hire(new Salaried("Fred"));
+        hr.hire(new Salaried());
+        hr.printEverybody();
     }
 }

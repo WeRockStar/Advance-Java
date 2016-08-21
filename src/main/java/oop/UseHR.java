4c496deb9a796923f275b0b874d3af622986dfbe
WeRockStar <vr_freestyle@outlook.com>
2016-08-21 21:49:35 +0700

    Test abstract class

--- src/main/java/oop/UseHR.java
+++ src/main/java/oop/UseHR.java
@@ -4,7 +4,11 @@ public class UseHR {
     public static void main(String[] args) {
         HR hr = new HR();
         hr.hire(new Salaried("Fred"));
+        hr.hire(new Salaried("Jee", 12324234));
+        hr.hire(new Hourly("Barney"));
         hr.hire(new Salaried());
+        hr.hire(new Hourly());
         hr.printEverybody();
+        hr.payEverybody();
     }
 }

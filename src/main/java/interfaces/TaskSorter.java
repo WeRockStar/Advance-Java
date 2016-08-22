3c035883dd73065d105778389172df7967a7019e
WeRockStar <vr_freestyle@outlook.com>
2016-08-22 22:08:45 +0700

    Display after sort

--- src/main/java/interfaces/TaskSorter.java
+++ src/main/java/interfaces/TaskSorter.java
@@ -1,13 +1,20 @@
 package interfaces;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TaskSorter {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         List<Task> tasks = new ArrayList<>();
         tasks.add(new Task(1, "Run away killer rabbit", 4));
         tasks.add(new Task(2, "Keep pokemon at Siam paragon", 1));
         tasks.add(new Task(3, "Evolve pokemon", 3));
+
+        // for (Task t : tasks) System.out.println(t);
+        tasks.forEach(System.out::println);
+        Collections.sort(tasks);
+        System.out.println("===========================");
+        tasks.forEach(System.out::println);
     }
 }

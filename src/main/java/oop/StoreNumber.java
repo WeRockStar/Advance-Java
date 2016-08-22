d59d27349959a6e3fc71755367c7883b412c1805
WeRockStar <vr_freestyle@outlook.com>
2016-08-23 00:01:30 +0700

    Added hasCode experiment

--- src/main/java/oop/StoreNumber.java
+++ src/main/java/oop/StoreNumber.java
@@ -12,10 +12,13 @@ public class StoreNumber {
         return numbers[num];
     }
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
         StoreNumber sn = new StoreNumber();
+        StoreNumber sn2 = new StoreNumber();
+        System.out.println("hasCode : " + sn.hashCode());
         System.out.println(sn.isStored(2));
         sn.store(2);
+        System.out.println("hasCode : " + sn2.hashCode());
         System.out.println(sn.isStored(2));
         System.out.println(sn.isStored(3));
         sn.store(3);

4cecef35e3546cbe18d730538ed098de61cc702f
WeRockStar <vr_freestyle@outlook.com>
2016-08-23 21:32:21 +0700

    Added one more generic

---
+++ src/main/java/generics/Tuple.java
@@ -0,0 +1,27 @@
+package generics;
+
+
+import java.text.MessageFormat;
+
+public class Tuple<T, U> {
+    private T first;
+    private U second;
+
+    public Tuple(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Tuple'{'first={0}, second={1}'}'", first, second);
+    }
+}
